424fbf7a5ff96b32e1a6e21c927303051dd81399
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-01 15:41:44 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Tells a student what their number grade would be given the letter grade
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    
+    // ask what letter grade they got
+    System.out.println("Please enter a letter grade");
+    String letterGrade = input.nextLine();
+
+    // tell them what grade they got
+    switch (letterGrade) {
+      case "A":
+        System.out.println("This is between 80% and 100%");
+        break;
+      case "B":
+        System.out.println("This is between 70% and 79%");
+        break;
+      case "C":
+        System.out.println("This is between 60% and 69%");
+        break;
+      case "D":
+        System.out.println("This is between 50% and 59%");
+        break;
+      case "F":
+        System.out.println("This is between 0% and 49%");
+        break;
+      default:
+        break;
+    }
     
   }
 }
